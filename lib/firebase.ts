3e587bf1659e439bd8ab55f28a523129869d3a77
jash-hub03 <2401021099@cgu-odisha.ac.in>
2026-04-18 23:58:39 +0530

    updated firebase config to env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8RUlI4HeaX2mrRrj13lohmd3f5TOcFFM",
-  authDomain: "mood-tracker345.firebaseapp.com",
-  projectId: "mood-tracker345",
-  storageBucket: "mood-tracker345.firebasestorage.app",
-  messagingSenderId: "1007046113746",
-  appId: "1:1007046113746:web:97d2cb51c586be2739700b",
-  measurementId: "G-CG9WJ1VQYQ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
